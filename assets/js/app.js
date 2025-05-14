f6401c4454b48179f9a0d16f0774c6857ab26862
Aarón López Sosa <aaron.sosa@unosquare.com>
2025-05-14 14:36:43 -0600

    update page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,53 +1,65 @@
-// configuracion para simplecount
-simplyCountdown("#cuenta", {
-  year: 2025, // required
-  month: 5, // required
-  day: 24, // required
-  hours: 3, // Default is 0 [0-23] integer
-  minutes: 0, // Default is 0 [0-59] integer
-  seconds: 0, // Default is 0 [0-59] integer
-  words: {
-    //words displayed into the countdown
-    days: { singular: "D", plural: "D" },
-    hours: { singular: "H", plural: "H" },
-    minutes: { singular: "M", plural: "M" },
-    seconds: { singular: "S", plural: "S" },
-  },
-  plural: false, //use plurals
-  inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
-  inlineClass: "simply-countdown-inline", //inline css span class in case of inline = true
-  // in case of inline set to false
-  enableUtc: true, //Use UTC or not - default : false
-  onEnd: function () {
-    return;
-  }, //Callback on countdown end, put your own function here
-  refresh: 1000, // default refresh every 1s
-  sectionClass: "simply-section", //section css class
-  amountClass: "simply-amount", // amount css class
-  wordClass: "simply-word", // word css class
-  zeroPad: false,
-  countUp: false,
-});
+window.addEventListener("load", () => {
+  const loader = document.querySelector("#loading-page");
+  loader.classList.add("hidden");
 
-// configuracion del plugin glide
-const config = {
-  type: "carousel",
-  perView: 1,
-  autoplay: 5000,
-};
+  const main = document.querySelector("#main-content");
+  main.classList.remove("content-hidden");
 
-new Glide(".glide", config).mount();
+  // configuracion para simplecount
+  simplyCountdown("#cuenta", {
+    year: 2025, // required
+    month: 5, // required
+    day: 24, // required
+    hours: 3, // Default is 0 [0-23] integer
+    minutes: 0, // Default is 0 [0-59] integer
+    seconds: 0, // Default is 0 [0-59] integer
+    words: {
+      //words displayed into the countdown
+      days: { singular: "D", plural: "D" },
+      hours: { singular: "H", plural: "H" },
+      minutes: { singular: "M", plural: "M" },
+      seconds: { singular: "S", plural: "S" },
+    },
+    plural: false, //use plurals
+    inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
+    inlineClass: "simply-countdown-inline", //inline css span class in case of inline = true
+    // in case of inline set to false
+    enableUtc: true, //Use UTC or not - default : false
+    onEnd: function () {
+      return;
+    }, //Callback on countdown end, put your own function here
+    refresh: 1000, // default refresh every 1s
+    sectionClass: "simply-section", //section css class
+    amountClass: "simply-amount", // amount css class
+    wordClass: "simply-word", // word css class
+    zeroPad: false,
+    countUp: false,
+  });
 
-const btnPlayPausa = document.getElementById("play-pausa");
-const audio = document.getElementById("miAudio");
-audio.currentTime = 0;
+  // configuracion del plugin glide
+  const config = {
+    type: "carousel",
+    perView: 1,
+    autoplay: 5000,
+  };
 
-btnPlayPausa.addEventListener("click", (event) => {
-  if (audio.paused) {
-    audio.play();
-    btnPlayPausa.innerHTML = "pause_circle";
-  } else {
-    audio.pause();
-    btnPlayPausa.innerHTML = "play_circle";
-  }
+  new Glide(".glide", config).mount();
+
+  const btnPlayPausa = document.getElementById("play-pausa");
+  const audio = document.getElementById("miAudio");
+  audio.currentTime = 0;
+
+  btnPlayPausa.addEventListener("click", (event) => {
+    if (audio.paused) {
+      audio.play();
+
+      btnPlayPausa.querySelector(".play").style.display = "none";
+      btnPlayPausa.querySelector(".pause").style.display = "block";
+    } else {
+      audio.pause();
+
+      btnPlayPausa.querySelector(".play").style.display = "block";
+      btnPlayPausa.querySelector(".pause").style.display = "none";
+    }
+  });
 });
